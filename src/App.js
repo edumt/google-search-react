53bddc7fb052889fc651b66a4a558570a1df1439
edumt <edumoura1997@gmail.com>
2021-09-07 21:10:06 -0300

    page skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <a href="#">Gmail</a>
+        <a href="#">Images</a>
+        <a href="#">Icon placeholder</a>
+        <a href="#">Profile pic placeholder</a>
       </header>
+      <main>
+        <p>Google logo</p>
+        <input type="text" name="" id="" />
+        <div>
+          <button>Google Search</button>
+          <button>I'm Feeling Lucky</button>
+        </div>
+      </main>
+      <footer>
+        <div>
+          <a href="#">About</a>
+          <a href="#">Advertising</a>
+          <a href="#">Business</a>
+          <a href="#">How Search works</a>
+        </div>
+        <div>
+          <a href="#">Privacy</a>
+          <a href="#">Terms</a>
+          <a href="#">Settings</a>
+        </div>
+      </footer>
     </div>
   );
 }
